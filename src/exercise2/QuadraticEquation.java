554889f4f66eb653b5a82401d7b411b6aa0b274f
Rostislav DImov <91912721+midrost@users.noreply.github.com>
2021-10-14 12:30:14 +0300

    Fix test error

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -5,39 +5,38 @@ import java.util.Scanner;
 public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner inputa = new Scanner(System.in);
-        int a = inputa.nextInt();
+        double a = inputa.nextDouble();
 
         Scanner inputb = new Scanner(System.in);
-        int b = inputb.nextInt();
+        double b = inputb.nextDouble();
 
         Scanner inputc = new Scanner(System.in);
-        int c = inputc.nextInt();
+        double c = inputc.nextDouble();
 
-        if (a == 0 && b == 0 && c == 0) {
-            System.out.println("Many values");
-            return;
-        } else if (a == 0 && b == 0 && c != 0) {
-            System.out.println("No values");
-            return;
-        }
-
-        if (a != 0) {
-            double diskriminant = b ^ 2 - 4 * a * c;
-            if (diskriminant < 0){
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Many values");
+                    return;
+                } else {
+                    System.out.println("No values");
+                }
+            } else {
+                double res = (-c) / b;
+                System.out.println(res);
+            }
+        } else {
+            double diskriminant = (b * b) - 4 * a * c;
+            if (diskriminant < 0) {
                 System.out.println("Imaginary values");
                 return;
             }
 
             double sqrt_dis = Math.sqrt(diskriminant);
-            double x1 = (-b + sqrt_dis)/2*a;
-            double x2 = (-b - sqrt_dis)/2*a;
+            double x1 = (-b + sqrt_dis) / 2 * a;
+            double x2 = (-b - sqrt_dis) / 2 * a;
 
             System.out.println(x1 + "\n" + x2);
-        }
-
-        if (a == 0 && b != 0){
-            double res = -c/b;
-            System.out.println(res);
         }
     }
 }
