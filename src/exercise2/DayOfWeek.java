e63443355a095bed6fc7fcc9b0221b77ab6fea86
Rostislav DImov <91912721+midrost@users.noreply.github.com>
2021-10-14 11:52:46 +0300

    Implement DayOfWeek

---
+++ src/exercise2/DayOfWeek.java
@@ -0,0 +1,41 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a natural number between 1 and 7: ");
+        int number = input.nextInt();
+
+        if (number < 1 || number > 7) {
+            System.out.println("Invalid day");
+            return;
+        }
+
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+        }
+    }
+}
